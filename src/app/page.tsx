1d0017d436f69189b8313928ae0eab3d8663b35b
Muhammad Shehroz Sarmad <shehrozm107@gmail.com>
2024-05-07 18:48:37 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSec from "@/components/HeroSec";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseus from "@/components/WhyChooseus";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className=" text-center text-2xl">
-        hello next js
-      </h1>
+      <HeroSec/>
+      <FeaturedCourses/>
+      <WhyChooseus/>
+      <TestimonialCards/>
+      <UpcomingWebinars/>
     </main>
     </>
   );
